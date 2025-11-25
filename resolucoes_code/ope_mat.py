d84e5f11d8c855689ad5195c181fc670f680f4df
Claudenir Rodrigues Araujo <62363683+nikkodayo@users.noreply.github.com>
2025-11-25 00:38:35 -0300

    Update ope_mat.py

--- resolucoes_code/ope_mat.py
+++ resolucoes_code/ope_mat.py
@@ -1,1 +1,21 @@
-# Vamos solicitar como entrada dois números e depois vamos realizar uma operação simples entre eles.
+# Solicitar como entrada dois números e depois vamos realizar uma operação simples entre eles.
+while True:
+  print('Selecione uma operacao:')
+  print('1 - Soma')
+  print('2 - Subtração')
+  print('3 - Multiplicão')
+  print('4 - Divisão')
+  print('0 - Encerrar')
+  op = int(input())
+  if op == 0:
+    break;
+  valor1 = int(input('Digite o valor 1: '))
+  valor2 = int(input('Digite o valor 2: '))
+  if op == 1:
+    print(valor1 + valor2)
+  elif op == 2:
+    print(valor1 - valor2)
+  elif op == 3:
+    print(valor1 * valor2)
+  elif op == 4:
+    print(valor1 / valor2)
