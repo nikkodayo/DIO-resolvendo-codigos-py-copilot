403182c2d9cc2d5a5932ca3ab8fe19e266599d50
Claudenir Rodrigues Araujo <62363683+nikkodayo@users.noreply.github.com>
2025-11-25 00:49:57 -0300

    Update repet_txt.py

--- resolucoes_code/repet_txt.py
+++ resolucoes_code/repet_txt.py
@@ -1,1 +1,5 @@
-# Vamos solicitar como entrada dois números e depois vamos realizar uma operação simples entre eles.
+# Solicita uma string e um número inteiro como entrada. Retorna a string repetida o número de vezes informado.
+palavra = input('Informe a string: ')
+n = int(input('Informe o número: '))
+for _ in range(n):
+  print(palavra)
