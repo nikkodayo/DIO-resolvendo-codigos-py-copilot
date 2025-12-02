177bf198ed514767b00492c70e8ce8c47d1d0d8b
Claudenir Rodrigues Araujo <62363683+nikkodayo@users.noreply.github.com>
2025-12-02 14:49:01 -0300

    Create media.py

---
+++ resolucoes_code/media.py
@@ -0,0 +1,5 @@
+valor1 = int(input('Digite a primeira nota: '))
+valor2 = int(input('Digite a segunda nota: '))
+valor3 = int(input('Digite a terceira nota: '))
+
+print('A média é: ',(valor1 + valor2 + valor3) / 3)
